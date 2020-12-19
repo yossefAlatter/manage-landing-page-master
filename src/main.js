c314ea65927b26877e3419e8a491596b757ba7df
yossefAlatter <yossefalatter@gmail.com>
2020-12-19 21:38:41 +0200

    first-commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//==import jquery==//
+import 'jquery';
+import 'jquery.nicescroll';
+
+
+
+//==import bootstrap==//
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+
+//==import fontAwesome ==//
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+//==import style file==//
+import './css/style.css';
+
 Vue.config.productionTip = false
 
 new Vue({
